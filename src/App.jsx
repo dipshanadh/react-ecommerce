5a9744cbcee865b40fda332355c3920b76262b0c
Dipshan Adhikari <dipshanadh@gmail.com>
2023-07-03 13:04:22 +0545

    Create All Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Login,
+  Orders,
+  Product,
+  Products,
+  Register,
+} from "./pages";
+
 const App = () => {
   return <h1 className="text-7xl font-bold underline">Hello, World!</h1>;
 };
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as About } from "./About";
+export { default as Cart } from "./Cart";
+export { default as Checkout } from "./Checkout";
+export { default as Error } from "./Error";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Orders } from "./Orders";
+export { default as Product } from "./Product";
+export { default as Products } from "./Products";
+export { default as Register } from "./Register";
