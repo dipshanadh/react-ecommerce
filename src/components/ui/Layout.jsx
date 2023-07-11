3c7a68138bcc69f6af277e2a6b72dd70c953a28b
Dipshan Adhikari <dipshanadh@gmail.com>
2023-07-11 10:09:45 +0545

    Navbar structure

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <main className="align-element py-20">
         <Outlet />
       </main>
---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,56 @@
+import { NavLink } from "react-router-dom";
+
+import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+import { FaBarsStaggered } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-base-200">
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          {/* Title */}
+          <NavLink
+            to="/"
+            className="hidden lg:block text-primary text-3xl font-semibold"
+          >
+            MK <span className="text-base-content">Store</span>
+          </NavLink>
+
+          {/* Dropdown */}
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h-6 w-6" />
+            </label>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-200 mt-3 rounded-box p-2 z-[1]"
+            >
+              navlinks
+            </ul>
+          </div>
+        </div>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">nav links</ul>
+        </div>
+
+        <div className="navbar-end">
+          {/* Theme icons */}
+
+          {/* Cart Link */}
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md">
+            <div className="indicator">
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-sm badge-primary indicator-item">
+                8
+              </span>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
